7134a6651aa1facb18ea95b6e80277661d8662a8
Susan Graves <susan@susangravescpa.com>
2017-01-21 18:34:52 -0500

    initial commit

---
+++ local_env.rb
@@ -0,0 +1,7 @@
+ENV['host'] = 
+# 'green-planet-prod.ccar1za80ci4.us-west-2.rds.amazonaws.com'
+'planetgreengames.c4ru0pfevyta.us-west-2.rds.amazonaws.com'
+ENV['port'] = '5432'
+ENV['db_name'] = 'postgres'
+ENV['user'] = 'planetgreengames'
+ENV['password'] = 'administrator'
